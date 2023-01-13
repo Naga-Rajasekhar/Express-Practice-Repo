52f11f3192e47d0686c9d385b92a8fedfea6b627
Naga-Rajasekhar <rajasekhar9860@gmail.com>
2023-01-13 22:19:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World, Raja");
+});
+
+app.get("/raja", (req, res)=>{
+    res.send("instagram");
+})
+
+app.get("/features", (req, res) => {
+  res.send("<h1>Test Runner Built for Humans</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
