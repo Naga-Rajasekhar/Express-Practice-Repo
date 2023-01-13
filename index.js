2d38dcbeb6b69e3f59770b1d882be4bc4697c260
Naga-Rajasekhar <rajasekhar9860@gmail.com>
2023-01-13 23:42:15 +0530

    leant about params

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World, Raja");
+  res.status(200).send("Hello World, Rajasekhar");
 });
 
 app.get("/raja", (req, res) => {
@@ -12,6 +12,24 @@ app.get("/raja", (req, res) => {
 
 app.get("/features", (req, res) => {
   res.send("<h1>Test Runner Built for Humans</h1>");
+});
+
+app.get("/features/followers", (req, res) => {
+  res.send("<h2>600K</h2>");
+});
+
+app.get("/example", (req, res) => {
+  const objExp = {
+    surName: "Naga",
+    firstName: "Raja",
+    lastName: "Sekhar",
+  };
+  res.status(200).json({ objExp });
+});
+
+app.get("/home/login/:token", (req, res) => {
+  console.log(req.params.token);
+  res.status(200).status({ param: req.params.token });
 });
 
 app.listen(port, () => {
